94d6d526750682f4989c7d7f5f7d1f6cdf4c56be
xinshoutw <me@xinshou.tw>
2025-07-13 08:59:54 +0800

    fix: runtime error

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const http = require('http');
 const {Server} = require("socket.io");
-
+const cors = require('cors');
 
 // --- 伺服器基本設定 ---
 const app = express();
